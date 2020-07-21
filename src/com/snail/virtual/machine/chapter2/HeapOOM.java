bc27f7be09d4c7cf30645e734c84270fd71aaa3b
fzbdeqq <438267278@qq.com>
2020-07-21 22:58:30 +0800

    虚拟机demo

---
+++ src/com/snail/virtual/machine/chapter2/HeapOOM.java
@@ -0,0 +1,17 @@
+package com.snail.virtual.machine.chapter2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM args:-Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ */
+public class HeapOOM {
+    static class OOMObject{}
+    public static void main(String[] args) {
+        List<OOMObject> list=new ArrayList<>();
+        while (true){
+            list.add(new OOMObject());
+        }
+    }
+}
